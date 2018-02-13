f5bd689c0361187d8cabf402c027b62636a540fc
Matt Fenwick <mfenwick100@gmail.com>
2018-02-13 16:51:04 -0500

    add alpine 3.2

--- run-in-oc.sh
+++ run-in-oc.sh
@@ -4,6 +4,7 @@ CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o echoer
 
 cp echoer centos/echoer
 cp echoer alpine/echoer
+cp echoer alpine3.2/echoer
 cp echoer alpine3.3/echoer
 cp echoer alpine3.4/echoer
 cp echoer alpine3.5/echoer
@@ -11,6 +12,7 @@ cp echoer alpine3.6/echoer
 
 docker build -t mfenwickbd/centos-echoer centos/
 docker build -t mfenwickbd/alpine-echoer alpine/
+docker build -t mfenwickbd/alpine-echoer-3.2 alpine-3.2/
 docker build -t mfenwickbd/alpine-echoer-3.3 alpine-3.3/
 docker build -t mfenwickbd/alpine-echoer-3.4 alpine-3.4/
 docker build -t mfenwickbd/alpine-echoer-3.5 alpine-3.5/
@@ -18,6 +20,7 @@ docker build -t mfenwickbd/alpine-echoer-3.6 alpine-3.6/
 
 docker push mfenwickbd/centos-echoer:latest
 docker push mfenwickbd/alpine-echoer:latest
+docker push mfenwickbd/alpine-echoer-3.2:latest
 docker push mfenwickbd/alpine-echoer-3.3:latest
 docker push mfenwickbd/alpine-echoer-3.4:latest
 docker push mfenwickbd/alpine-echoer-3.5:latest
@@ -29,6 +32,7 @@ oc new-project mff
 oc project mff
 oc new-app mfenwickbd/centos-echoer:latest
 oc new-app mfenwickbd/alpine-echoer:latest
+oc new-app mfenwickbd/alpine-echoer-3.2:latest
 oc new-app mfenwickbd/alpine-echoer-3.3:latest
 oc new-app mfenwickbd/alpine-echoer-3.4:latest
 oc new-app mfenwickbd/alpine-echoer-3.5:latest
